e41e70bb34d0b74dc0f5fa60174e83eef55e90a7
Ravish Bisht <ravishbisht85.com>
2024-11-08 18:17:24 +0530

    global error handling add

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import app from "./src/app";
 import { config } from "./src/config/config";
 import connectDB from "./src/config/db";
 
-const startServer = async() => {
+const startServer = async () => {
   await connectDB();
   const port = config.port || 3000;
 
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 //Routes
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.get("/", (req,res,next)=>{
-    res.json({message:"welcome to elib apis"})
-})
+app.get("/", (req, res, next) => {
+  res.json({ message: "welcome to elib apis" });
+});
 
+app.use(globalErrorHandler);
 
 export default app;
