d4edd0d703105b7dc8fc8e9bed9170a476d76e1d
Ravish Bisht <ravishbisht85.com>
2024-11-08 19:09:12 +0530

    add user register validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  //validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  //process
+
+  //response
+
   res.json({ message: "user created" });
 };
 
