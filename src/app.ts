9267ab56c4d246fd334ade3f7df1a61aac540616
Ravish Bisht <ravishbisht85.com>
2024-11-08 18:59:02 +0530

    user router created

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 //Routes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 app.get("/", (req, res, next) => {
   res.json({ message: "welcome to elib apis" });
 });
 
+app.use("/api/users", userRouter);
+
+//global error handler
 app.use(globalErrorHandler);
 
 export default app;
